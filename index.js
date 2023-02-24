481ee8a1ad58f8536f5abe5b3b6a0d3381688b3e
Matt Phan <mttphan261@gmail.com>
2023-02-23 19:37:43 -0600

    commit tree 46ff3884ff864011a89fcb6e0fea13f73e4d5b53

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Matthew",
+    streetAddress: "1234 Sesame St",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; 
+
+    return obj;
+    };
+
+function deleteFromEmployeeByKey(obj, key, value) {
+    const newObj = { ...obj};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+   delete employee[key]
+   return employee;
+}
